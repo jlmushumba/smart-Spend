1bf8a362a58211ce3cdf29decf1063f05621cbec
jlmushumba <jlmushumba@gmail.com>
2026-08-07 16:18:35 +0200

    Refined user profile functions

--- services/user_service.py
+++ services/user_service.py
@@ -1,37 +1,54 @@
 import json
 from models.user import User
+from sys import exit
 
-def create_profile(user_name, user_email, user_currency):
+def create_profile(user_name = None, user_email = None, user_currency = None):
 
     """
     Creates new profile and saves it in assets/profile.json
     """
+   
+    try:
 
-    user = User(user_name, user_email, user_currency)
-    data = user.to_dict()
-    with open ("data/profile.json", "w") as file:
-        json.dump(data, file, indent=4)
+        user = User(user_name, user_email, user_currency)
+        data = user.to_dict()
+        with open ("data/profile.json", "w") as file:
+            json.dump(data, file, indent=4)
+    except FileNotFoundError as error:
+        print(f"Error: {error}")
 
 def view_profile():
     """
     It views the content of models/profile.json (profile)
     """
-    with open ("data/profile.json", "r") as file:
-        contents = json.load(file)
-    return contents
+    if not "data/profile.json":
+        raise ValueError('No profile found!')
+    try:
 
-def edit_profile(name = None, email = None, currency= None):
+        file = open ("data/profile.json", "r") 
+        contents = json.load(file)
+        return contents
+    except FileNotFoundError as e1:
+        print(f"Error: {e1}")
+    
+
+def edit_profile(data: dict):
     """
     It changes the user's pre-existing data accordingly
     """
-    if not edit_profile:
-        return None
-    
-    new_profile = {
-        "name" : name,
-        "email" : email,
-        "currency" : currency
-    }
 
-    with open ("data/profile.json", "w") as file:
-        json.dump(new_profile, file, indent=4)
+    try:
+
+        with open ("data/profile.json", "r+") as file :
+            contents = json.load(file)
+
+            for key, value in data.items():
+                contents[key] = value
+            file.seek(0)
+            json.dump(contents, file, indent=4)
+            file.truncate()
+
+        
+
+    except FileNotFoundError as error:
+        print(f"Error: {error}")
