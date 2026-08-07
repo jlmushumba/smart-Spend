bb4289491d0d4ec83a1fd3e24937ee6340c50746
jlmushumba <jlmushumba@gmail.com>
2026-08-07 16:20:43 +0200

    Added: function to edit user data

--- utils/helpers.py
+++ utils/helpers.py
@@ -15,4 +15,28 @@ def read_file(path: str):
 
     except Exception as error:
         print(f"{error}")
+
+
+def edit_profile_data():
+    data_to_edit = print(
+                    "What do you want to change ? (Enter the corresponding number) \n 1. name\n 2. email 3. currency"
+                    )
+    edit_choice = input(">> ")
+
+    if edit_choice == "1":
+        new_name = input("Enter new name: ")
+        return {"name" : new_name}
+        
+
+    elif edit_choice == "2":
+        new_email = input("Enter your new email: ")
+        return {"email" : new_email}
+
+    elif edit_choice == "3":
+        new_currency = input("Enter new currency: ")
+        return {"currency" : new_currency}
+
+    else:
+        print("Invalid Input!")
+
     
