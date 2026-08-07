02b291ad61a3c748b787ca4f0b59e3af2de77b75
jlmushumba <jlmushumba@gmail.com>
2026-08-07 16:22:09 +0200

    Created: Menu displayers

---
+++ utils/menu.py
@@ -0,0 +1,26 @@
+def main_menu():
+    with open ("assets/main_menu.txt", "r") as file:
+        menu = file.read()
+        return menu
+
+
+def user_profile_menu():
+    with open ("assets/user_profile.txt", "r") as file:
+        user_profile = file.read()
+        return user_profile
+    
+
+def income_menu():
+    pass
+
+def expenses_menu():
+    pass
+def budget_menu():
+    pass
+
+def reports_menu():
+    pass
+
+if __name__ == "__main__":
+
+    print(user_profile_menu())
