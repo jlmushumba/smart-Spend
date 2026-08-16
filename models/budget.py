88ef455452987ccfc87b7ded51de530f07b2152b
jlmushumba <jlmushumba@gmail.com>
2026-08-16 22:18:12 +0200

    Created: budget class

---
+++ models/budget.py
@@ -0,0 +1,16 @@
+class Budget:
+
+    def __int__(self, id, category, monthly_limit, month_year):
+        self.id = id
+        self.category = category
+        self.monthly_limit = monthly_limit
+        self.month_year = month_year
+
+    def to_dict(self):
+
+        return {
+            "id": self.id,
+            "category": self.category,
+            "monthly_limit": self.monthly_limit,
+            "month_year": self.month_year
+        }
