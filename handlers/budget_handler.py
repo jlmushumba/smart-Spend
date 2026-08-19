eb5e151ff83b5be9f30d9f05ff1ac43d575492d9
jlmushumba <jlmushumba@gmail.com>
2026-08-19 13:30:18 +0200

    Implemented add budget and view budget functions

--- handlers/budget_handler.py
+++ handlers/budget_handler.py
@@ -1,10 +1,77 @@
 from datetime import datetime
-from utils.helpers import read_file
-from services.user_service import edit_profile_data
-from services.income_service import add_income_parameters, delete_income, edit_income
-import sys
-from services.user_service import (create_profile, view_profile,edit_profile)
-from utils.menu import (
-    main_menu, user_profile_menu, income_menu, expenses_menu, budget_menu, reports_menu
-    )
-from services.income_service import (add_income, view_income)
+from utils.menu import budget_menu
+from services.budget_service import (
+    add_budget, view_budget, edit_budget, delete_budget
+)
+from utils.helpers import recent_id
+
+def handle_budget():
+
+    print(budget_menu())
+
+    while True:
+
+        budget_choice = input(">> ")
+
+        if budget_choice == "1": # set budget
+
+            # id 
+            id = recent_id("data/budgets.json")
+
+            # category
+
+            category = input("Enter category: ")
+
+            # monthly limit money
+
+            while True:
+                try:
+
+                    monthly_limit = int(input("Enter money monthly limit: "))
+                    break
+                except ValueError:
+                    print("Please enter a valid amount of money in numbers!")
+                except Exception as error:
+                    print(f"Error: {error}")
+
+            # month/year of setting the monthly budget
+
+            while True:
+                budget_date = input("Enter budget month (MM/YYYY): ")
+                try:
+                                                           
+                    date_object = datetime.strptime(budget_date, "%m/%Y").date()
+                    formatted_date = date_object.strftime("%m/%Y")
+                    month_year = formatted_date
+                    break
+                 
+                except ValueError:
+                    print("Invalid date format! Please enter in MM/YYYY format (e.g., 08/2026)")
+                except Exception as e1:
+                    print(f"Error: {e1}")
+
+            add_budget(id, category, monthly_limit, month_year)
+            print(budget_menu())
+           
+
+        elif budget_choice == "2": # view budget
+            budgets = view_budget()
+
+            for budget in budgets:
+                print("=" * 45)
+                for key, value in budget.items():
+                    print(f"{key} : {value}")
+            
+            print(budget_menu())
+
+        elif budget_choice == "3": # edit budget by ID
+            edit_budget()
+            pass
+
+        elif budget_choice == "4": # Delete budget by ID
+            delete_budget()
+            pass
+
+        elif budget_choice == "5": # back to main menu
+            break
+        
