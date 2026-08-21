cbc6e4cfc5b4317688fc2dac52cf2611d615ded7
jlmushumba <jlmushumba@gmail.com>
2026-08-22 00:43:09 +0200

    Implemented delete budget and edit budget functions

--- handlers/budget_handler.py
+++ handlers/budget_handler.py
@@ -65,11 +65,88 @@ def handle_budget():
             print(budget_menu())
 
         elif budget_choice == "3": # edit budget by ID
-            edit_budget()
-            pass
+            # TODO "
+            # - gather parameters for edit function"
+            edit_choices = {}
+
+            while True: 
+
+                try:
+                    edit_id = int(input("Enter udget ID to edit: "))
+                    break
+                except ValueError:
+                    print("Please enter an existing ID!")
+            
+            print("Choose what to edit(when done press 4): ")
+            while True:
+
+                
+                print("1. Category \n2. Monthly limit \n3. Month")
+                print("=" * 45)
+                print("When done press 4 !")
+                print("=" * 45)
+                print()
+                choice = input(">> ")
+
+                if choice == "1":
+                    # category
+                    new_category = input("Enter new category: ")
+                    edit_choices["category"] = new_category
+                    
+
+                elif choice == "2":
+                    #monthly limit
+                    while True:
+                        try:
+
+                            new_monthly_limit = int(input("Enter money monthly limit: "))
+                            break
+                        except ValueError:
+                            print("Please enter a valid amount of money in numbers!")
+                        except Exception as error:
+                            print(f"Error: {error}")
+                    edit_choices["monthly_limit"] = new_monthly_limit
+                            
+                elif choice == "3":
+                    # month
+                    while True:
+                        new_budget_date = input("Enter budget month (MM/YYYY): ")
+                        try:
+                                                                
+                            date_object = datetime.strptime(new_budget_date, "%m/%Y").date()
+                            formatted_date = date_object.strftime("%m/%Y")
+                            new_month_year = formatted_date
+                            break
+                        
+                        except ValueError:
+                            print("Invalid date format! Please enter in MM/YYYY format (e.g., 08/2026)")
+                        except Exception as e1:
+                            print(f"Error: {e1}")
+
+                    edit_choices["month_year"] = new_month_year
+
+                elif choice == "4":
+                    #execute
+                    edit_budget(edit_choices, edit_id)
+                    break
+                    
+
+                else:
+                    print("Invalid input!")
+            print(budget_menu())
 
         elif budget_choice == "4": # Delete budget by ID
-            delete_budget()
+            # TODO "
+            # - gather parameters for delete_budget"
+            while True: 
+
+                try:
+                    id_to_delete = int(input("Enter udget ID to edit: "))
+                    break
+                except ValueError:
+                    print("Please enter an existing ID!")
+            delete_budget(id_to_delete)
+            print(budget_menu())
             
 
         elif budget_choice == "5": # back to main menu
